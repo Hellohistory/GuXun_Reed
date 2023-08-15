539ed9828fca80a0cd5a48a49afcd7bfe956453d
Hellohistory <85825011+Hellohistory@users.noreply.github.com>
2023-08-15 22:11:56 +0800

    增加进程池功能，增加下载速度

--- mainwindow_module/download_images.py
+++ mainwindow_module/download_images.py
@@ -1,47 +1,40 @@
 import os
-
+from PyQt5.QtCore import QRunnable, pyqtSignal, QObject, QThreadPool
+from PyQt5.QtWidgets import QAbstractItemView, QDialog, QVBoxLayout, QListWidget, QPushButton, QFileDialog, QMessageBox
 import requests
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QAbstractItemView
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QListWidget, QPushButton
-from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtWidgets import QMessageBox
 
 
-class DownloadThread(QThread):
-    download_progress = pyqtSignal(str, int)  # Signal to report individual file download progress
-    download_complete = pyqtSignal(list, list)  # Signal to report all files download completion
+class DownloadSignals(QObject):
+    download_result = pyqtSignal(str, bool)  # Signal to report individual file download result
 
-    def __init__(self, image_browser, selected_images, save_directory):
+
+class DownloadTask(QRunnable):
+    def __init__(self, image_data, save_directory):
         super().__init__()
-        self.image_browser = image_browser
-        self.selected_images = selected_images
+        self.image_data = image_data
         self.save_directory = save_directory
+        self.signals = DownloadSignals()
 
     def run(self):
-        successful_downloads = []
-        failed_downloads = []
-        for idx, image_name in enumerate(self.selected_images):
-            image_data = self.image_browser.get_image_data_by_name(image_name)
-            image_id, extension, file_name = image_data
-            url = f"https://picshack.net/ib/download/{image_id}"
-            response = requests.get(url)
-            if response.status_code == 200:
-                file_path = os.path.join(self.save_directory, f"{file_name}.{extension}")
-                with open(file_path, 'wb') as file:
-                    file.write(response.content)
-                successful_downloads.append(file_name)
-            else:
-                failed_downloads.append(file_name)
-            self.download_progress.emit(image_name, idx + 1)  # Emit progress signal
-
-        self.download_complete.emit(successful_downloads, failed_downloads)  # Emit completion signal
-
+        image_id, extension, file_name = self.image_data
+        url = f"https://picshack.net/ib/download/{image_id}"
+        response = requests.get(url)
+        success = False
+        if response.status_code == 200:
+            file_path = os.path.join(self.save_directory, f"{file_name}.{extension}")
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+            success = True
+        self.signals.download_result.emit(file_name, success)  # Emit result signal
 
 class DownloadManager:
+    MAX_CONCURRENT_DOWNLOADS = 10  # Maximum number of concurrent downloads
+
     def __init__(self, main_window, image_browser):
         self.main_window = main_window
         self.image_browser = image_browser
+        self.thread_pool = QThreadPool()
+        self.thread_pool.setMaxThreadCount(self.MAX_CONCURRENT_DOWNLOADS)
 
     def download_images(self):
         dialog = QDialog(self.main_window)
@@ -59,27 +52,32 @@ class DownloadManager:
         dialog.resize(700, 500)
 
         if dialog.exec_():
-            selected_images = [item.text() for item in list_widget.selectedItems()]
+            selected_images = [self.image_browser.get_image_data_by_name(item.text()) for item in list_widget.selectedItems()]
             save_directory = QFileDialog.getExistingDirectory(self.main_window, "选择保存目录")
 
-            # Create and configure the download thread
-            self.download_thread = DownloadThread(self.image_browser, selected_images, save_directory)
-            self.download_thread.download_progress.connect(self.on_download_progress)
-            self.download_thread.download_complete.connect(self.on_download_complete)
-            self.download_thread.start()
+            self.successful_downloads = []
+            self.failed_downloads = []
+            self.remaining_downloads = len(selected_images)
+
+            for image_data in selected_images:
+                download_task = DownloadTask(image_data, save_directory)
+                download_task.signals.download_result.connect(self.on_download_result)
+                self.thread_pool.start(download_task)
+
+    def on_download_result(self, file_name, success):
+        if success:
+            self.successful_downloads.append(file_name)
         else:
-            QMessageBox.warning(self.main_window, "取消下载", "下载已取消")
+            self.failed_downloads.append(file_name)
+        self.remaining_downloads -= 1
 
-    def on_download_progress(self, image_name, idx):
-        # You can update a progress bar or status message here
-        pass
-
-    def on_download_complete(self, successful_downloads, failed_downloads):
-        if successful_downloads:
-            success_message = "\\n".join(successful_downloads)
-            QMessageBox.information(self.main_window, "下载成功",
-                                    f"以下图像已成功下载:\\n{success_message}")
-        if failed_downloads:
-            failure_message = "\\n".join(failed_downloads)
-            QMessageBox.warning(self.main_window, "下载失败",
-                                f"以下图像下载失败，请稍后重试:\\n{failure_message}")
+        # Report overall download result when all threads are done
+        if self.remaining_downloads == 0:
+            if self.successful_downloads:
+                success_message = "\\n".join(self.successful_downloads)
+                QMessageBox.information(self.main_window, "下载成功",
+                                        f"以下图像已成功下载:\\n{success_message}")
+            if self.failed_downloads:
+                failure_message = "\\n".join(self.failed_downloads)
+                QMessageBox.warning(self.main_window, "下载失败",
+                                    f"以下图像下载失败，请稍后重试:\\n{failure_message}")
